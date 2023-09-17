5efd8187115e6c5cb50934e7fd383c4edac6c36a
hibiki <75057530+shimanakahibiki@users.noreply.github.com>
2023-09-17 16:22:43 +0900

    ローカルで作成したBBSアプリの初期コミット

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # localhost:3000/posts/new
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
+end
